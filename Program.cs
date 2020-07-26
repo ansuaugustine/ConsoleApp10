ef5a662554c62dbcf9936b8eacf7330cac7d45ff
Ansu Augustine <200451181@student.georgianc.on.ca>
2020-07-26 17:49:23 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+            using System;
+            using System.IO;
+            using System.Net.Http;
+            using System.Net.Http.Headers;
+            using System.Threading.Tasks;
+
+namespace CVSPredictionSample
+    {
+        public static class Program
+        {
+            public static void Main()
+            {
+                Console.Write("Enter image file path: ");
+                string imageFilePath = Console.ReadLine();
+
+                MakePredictionRequest(imageFilePath).Wait();
+
+                Console.WriteLine("\n\nHit ENTER to exit...");
+                Console.ReadLine();
+            }
+
+            public static async Task MakePredictionRequest(string imageFilePath)
+            {
+                var client = new HttpClient();
+
+                // Request headers - replace this example key with your valid Prediction-Key.
+                client.DefaultRequestHeaders.Add("Prediction-Key", "4bcb1cdbb4d54bca9013e05f4a026a4c");
+
+                // Prediction URL - replace this example URL with your valid Prediction URL.
+                string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/83d0a94e-dea7-48c4-95f7-2d9e390d0480/classify/iterations/Iteration1/image";
+
+                HttpResponseMessage response;
+
+                // Request body. Try this sample with a locally stored image.
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                using (var content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    response = await client.PostAsync(url, content);
+                    Console.WriteLine(await response.Content.ReadAsStringAsync());
+                }
+            }
+
+            private static byte[] GetImageAsByteArray(string imageFilePath)
+            {
+                FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+    }
+    
